524703e9ae7c17d365e2b78cec948ca4f1211b4e
Chanissa Trithipkaiwanpon <chanissat@Chanissas-MacBook-Pro-2.local>
2019-03-19 09:08:39 +0700

    merge

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -5,7 +5,6 @@ export function BarChart(props) {
 	//props = {data,color}
 	const {data,color,YAxis} = props;
 	return (
-<<<<<<< HEAD
 		<ResponsiveContainer width="80%" height="80%" >
 			<Chart data={data}>
 				<CartesianGrid strokeDasharray="3 3" />
@@ -17,14 +16,6 @@ export function BarChart(props) {
 					console.log(e)
 					return <Bar dataKey={e.dataKey} fill={e.fill}/>
 				})}
-=======
-		<ResponsiveContainer width="100%" height="80%" style={{fontSize: '11px'}}>
-			<Chart data={props.data}>
-				<CartesianGrid strokeDasharray="1 1" />
-				<XAxis dataKey="name" />
-				<YAxis dataKey="value" />
-				<Bar dataKey="value" fill="#8884d8" />
->>>>>>> 27675fb09bdadc57953e3d705a4ff340d3c5c778
 			</Chart>
 		</ResponsiveContainer>
 	);
