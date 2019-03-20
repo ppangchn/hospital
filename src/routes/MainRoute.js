8b2760f1f0cff5a6e4ec146542fbf05e84d987e2
Nutchanon Ploypray <mrearth25@gmail.com>
2019-03-20 14:50:53 +0700

    add Staff

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import OperationalDashboard from '../views/operational-dashboard';
 import Historical from '../views/tactic-dashboard/historical';
 import Scenario from '../views/tactic-dashboard/scenario';
+import StaffDashboard from '../views/staff';
 
 class MainRoute extends Component {
 	render() {
 		return (
 				<Switch>
 					<Route exact path="/" component={OperationalDashboard} />
+					<Route path="/staff" component={StaffDashboard}/>
 					<Route path="/tactic/history" component={Historical} />
 					<Route path="/tactic/scenario" component={Scenario} />
 				</Switch>
