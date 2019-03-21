3c9c6919516ea8c8c55ae9503c04371d5d6f4519
Nutchanon Ploypray <mrearth25@gmail.com>
2019-03-21 13:19:01 +0700

    realtime

--- src/views/operational-dashboard/Header.js
+++ src/views/operational-dashboard/Header.js
@@ -2,17 +2,28 @@ import React, { Component } from "react";
 import "../../css/operational-dashboard/Header.css";
 import "../../css/Layout.css";
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
+    console.log(this.props);
     return (
       <div className="d-flex bd-highlight  mb-2">
         <div className="p-2 bd-highlight  " style={{ width: "100px" }}>
           <div className="flex-fill header-container  d-flex align-items-center justify-content-center">
-          
-        </div>
+            {this.props.isLoading ? (
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
         <div className="p-2 flex-grow-1 bd-highlight row">
           <div className="col header-container ml-1 ">รอคิว</div>
-          <div className="col header-container ml-1 " >กำลังทำงาน</div>
+          <div className="col header-container ml-1 ">กำลังทำงาน</div>
         </div>
       </div>
     );
--- src/views/operational-dashboard/PrepareInProgress.js
+++ src/views/operational-dashboard/PrepareInProgress.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import "../../css/operational-dashboard/OperationalDashboard.css";
 import { BarChart } from "../../components/BarChart";
+import Axios from "axios";
+
 
 const InQueue = styled.div`
   background-color: rgb(151,210,214);
@@ -14,6 +16,17 @@ const Queue = styled.div`
   background-color: white;
   box-shadow: 1px 3px 6px 2px rgb(180, 180, 180);
 `
+const Queue1 = styled.div`
+  width: 80px;
+  height: 40px;
+  background-color: white;
+  box-shadow: 1px 3px 6px 2px rgb(180, 180, 180);
+`;
+const Queue2 = styled.div`
+  width: 80px;
+  height: 40px;
+  font-size: 15px;
+`;
 const MyChart = styled.div`
    background-color: white;
    height: 90%;
@@ -31,6 +44,32 @@ class PrepareInProgress extends Component {
     super(props);
     this.state = {};
   }
+
+  getStaff(){
+    let fullTime = 0;
+    let partTime =0;
+    let other = 0;
+    this.props.pick.forEach(pre => {
+      if(pre.o_type === 1){
+        if(pre.parttime === 2) partTime ++;
+        else if(pre.parttime === 1){
+          if(pre.o_id === 99) other++;
+          else fullTime ++;
+        }
+      }
+    })
+    return [fullTime,partTime,other]
+  }
+
+  async componentDidMount(){
+    const res = await Axios.get('http://localhost:5001/getStaff')
+    const {data} = res;
+    const staff = data[0]
+    this.setState({staff})
+    console.log('staff',this.state);
+  }
+
+
   render() {
     return (
       <InQueue className="col header-container cell ml-1">
@@ -41,9 +80,71 @@ class PrepareInProgress extends Component {
               <Queue className="d-flex justify-content-center align-items-center">{this.props.pick.length}</Queue>
             </div>
           </div>
-          <ChartFrame className="col-9 cell d-flex justify-content-center align-items-center">
-
-          </ChartFrame>
+          <div className="col-9 cell">
+            <div className="row mt-4">
+              <div className="col">เจ้าหน้าที่</div>
+            </div>
+            <div className="row">
+              <div className="col-7">
+                {" "}
+                <span style={{ fontSize: "12px" }}>กำลังทำงาน</span>{" "}
+              </div>
+              <div className="col-5">
+                {" "}
+                <span style={{ fontSize: "12px" }}>ทั้งหมด</span>{" "}
+              </div>
+            </div>
+            <div className="row ">
+              <div className="col-7 ">
+                <div className="row d-flex justify-content-center align-items-center mr-1">
+                  <Queue1 className="d-flex justify-content-center align-items-center mr-1">
+                    {this.getStaff()[0]}
+                  </Queue1>
+                  <Queue1 className="d-flex justify-content-center align-items-center mr-1">
+                  {this.getStaff()[1]}
+                  </Queue1>
+                  <Queue1 className="d-flex justify-content-center align-items-center mr-1">
+                  {this.getStaff()[2]}
+                  </Queue1>
+                </div>
+              </div>
+              <div className="col-5">
+                <div className="row d-flex justify-content-center align-items-center mr-1">
+                  <Queue1 className="d-flex justify-content-center align-items-center mr-1">
+                  {this.state.staff ? this.state.staff.full_pick : 0}
+                  </Queue1>
+                  <Queue1 className="d-flex justify-content-center align-items-center mr-1">
+                  {this.state.staff ? this.state.staff.part_pick: 0}
+                  </Queue1>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-1">
+              <div className="col-7 ">
+                <div className="row d-flex justify-content-center align-items-center mr-1">
+                  <Queue2 className="d-flex justify-content-center align-items-center mr-1">
+                    Full-time
+                  </Queue2>
+                  <Queue2 className="d-flex justify-content-center align-items-center mr-1">
+                    Part-time
+                  </Queue2>
+                  <Queue2 className="d-flex justify-content-center align-items-center mr-1">
+                    อื่น
+                  </Queue2>
+                </div>
+              </div>
+              <div className="col-5">
+                <div className="row d-flex justify-content-center align-items-center mr-1">
+                  <Queue2 className="d-flex justify-content-center align-items-center mr-1">
+                    Full-time
+                  </Queue2>
+                  <Queue2 className="d-flex justify-content-center align-items-center mr-1">
+                    Part-time
+                  </Queue2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </InQueue>
     );
